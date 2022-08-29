bb1a45acdf32bc36f5ee14239dc357d7fe954be1
Kittaphatt <kittaphatc63@nu.ac.th>
2022-08-29 22:48:50 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,11 +2,12 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/s"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjU0MTUxLCJleHAiOjE2NTkyNTc3NTEsIm5iZiI6MTY1OTI1NDE1MSwianRpIjoibVVyUWw0T1p6U3dMd2lPeCIsInN1YiI6MTA5LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.fEFbkpgSvo4Hg1L7_oNCuSJ_r3-4g14x4lwMsfl2960"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzg3NjY0LCJleHAiOjE2NjE3OTEyNjQsIm5iZiI6MTY2MTc4NzY2NCwianRpIjoiZXZwcWZ3T3V3VHJoN3hxRyIsInN1YiI6MTA5LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.0-inL41wIoFiYQvfyRealC3Srjw3mVXAEi89aVumkSM"; // put access token
 const student = {
-  name: '', // replace with your full name.
+  id_student:63360194,
+  name: 'Kittaphat', // replace with your full name.
   age: 19, // put your age.
-  gender: '', // replace with your gender
+  gender: 'Male', // replace with your gender
   department: 'CPE'
 };
 
@@ -17,7 +18,7 @@ exports.findStudentbyId = function (student_id, cb) {
             method: "GET",
             uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer ${accessToken}`
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzg3ODM3LCJleHAiOjE2NjE3OTE0MzcsIm5iZiI6MTY2MTc4NzgzNywianRpIjoiUW9zRjJmSXpOTnJPVmxqUiIsInN1YiI6MTA5LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.kRHurocS5g14LsBfHWFzffXF9AF6e0j-erf7vXrrqaI`
             }
         }).then((response) => {
             console.log('Sent');
